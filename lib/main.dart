dc4bf893fe9001bbf90082b5c3cc1ccfc73ce54c
Karen Gomes <karenecris12015@gmail.com>
2024-10-21 15:56:50 -0300

    icon de estrela

--- lib/main.dart
+++ lib/main.dart
@@ -74,12 +74,27 @@ class _TaskState extends State<Task> {
                         child: Image.network( widget.foto,
                               fit: BoxFit.cover,),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(widget.nome, style: TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis
-                          ),)
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: 200,
+                              child: Text(widget.nome, style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis
+                              ),)
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star, size: 15, color: Colors.blue),
+                              Icon(Icons.star, size: 15, color: Colors.blue),
+                              Icon(Icons.star, size: 15, color: Colors.blue),
+                              Icon(Icons.star, size: 15, color: Colors.blue[100]),
+                              Icon(Icons.star, size: 15, color: Colors.blue[100]),
+                            ],
+                          )
+                        ],
                       ),
                       Container(
                         height: 52,
