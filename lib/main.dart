456f73f83eb8e2970f617a1a26f82b371dc37d71
Karen Gomes <karenecris12015@gmail.com>
2024-10-21 14:24:20 -0300

    atividade extra

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,42 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      // home: Container( //Exercício Alura
+      //   color: Colors.black,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Container(color: Colors.red, height: 100, width: 100,),
+      //           Container(color: Colors.orange, height: 100, width: 100,),
+      //           Container(color: Colors.yellow, height: 100, width: 100,)
+      //         ],
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Container(color: Colors.green, height: 100, width: 100,),
+      //           Container(color: Colors.cyan, height: 100, width: 100,),
+      //           Container(color: Colors.blue, height: 100, width: 100,)
+      //         ],
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Container(color: Colors.purple, height: 100, width: 100,),
+      //           Container(color: Colors.pinkAccent, height: 100, width: 100,),
+      //           Container(color: Colors.white, height: 100, width: 100,)
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      // ),
       home: Container(
         color: Colors.white,
         child: Column(
