b360f5e10049afeeb8c335a7a181ea1fd5843ed2
Karen Gomes <karenecris12015@gmail.com>
2024-10-21 15:01:22 -0300

    Criação de widget Task

--- lib/main.dart
+++ lib/main.dart
@@ -17,56 +17,58 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas')
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(color: Colors.red, width: 100, height: 100,),
-                  Container(color: Colors.blue, width: 50, height: 50,)
-                ],
-              ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100,),
-                Container(color: Colors.blue, width: 50, height: 50,)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.cyan, height: 50, width: 50),
-                Container(color: Colors.pinkAccent, height: 50, width: 50),
-                Container(color: Colors.purple, height: 50, width: 50)
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Diamante amarelo',
-                style: TextStyle(color: Colors.black, fontSize: 28),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  print('Você apertou o botão');
-                },
-                child: Text('Aperte o botão')
-            )
+            Task('Aprender Flutter'),
+            Task('Andar de bike'),
+            Task('Meditar'),
           ],
         ),
+        floatingActionButton: FloatingActionButton(onPressed: (){}),
       )
     );
   }
 }
+
+class Task extends StatelessWidget {
+  final String nome;
+
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 
 
